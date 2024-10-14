3ef5dab51dbfbf666104430e71a2a9fc02b62600
heinriej <heinirej@mail.uc.edu>
2024-10-14 12:19:30 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        Random rnd = new Random();
+
+        int die1 = 0;
+        int die2 = 0;
+        int crapsRoll = 0;
+
+        int die1 = rnd.nextInt(6)+1;
+    }
+}
